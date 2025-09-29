d85765a57787c45c3c4e38a44a42ed7f9020b4a0
HaChungLuong <hachungluongz2004@luongz2004gmail.z2004gmailcom>
2025-09-29 11:32:28 +0700

    29/99

---
+++ 2221050318/Bai 6 Javascrip/script.js
@@ -0,0 +1,64 @@
+// document.writeln("Hello World" + "<br>");
+
+        // // để khai báo 
+        // //
+        // let hoTen = ("Ha Chung Luong" + "<br>");
+
+        // document.writeln(hoTen);
+
+        // let soThuNhat = 10;
+        // let soThuHai = 5;
+        // let ketQua = 0;
+
+        // Cộng (+); Trừ (-); Nhân (*); Chia(/)
+
+        // Tăng 1: ++
+        // Giảm 1: --
+        
+        //ketQua = soThuNhat / soThuHai;
+
+        // soThuNhat + soThuHai = KQ => (soThuNhat += soThuHai);
+        //soThuNhat %= soThuHai;
+
+        // let soThuNhat = 10;
+        // let soThuHai = 5;
+        // let ketQua = 0;
+
+        // == != True false; > < >= <= SD ở câu điều kiện
+        // === thỏa mãn giá trị
+        // !== phủ dịnh kết quả
+
+        // let soThuNhat = 10;
+        // let soThuHai = 5;
+        // let ketQua = 0;
+
+
+        // let thongTinSinhVien = {
+        //     hoTen: "Ha Chung Luong",
+        //     mssv: "2221050318",
+        //     gioiTinh: true,
+        //     tuoi: 21,
+        //     lop: "Đại học_Tin kinh tế_K67A"
+        // }
+
+        // let diaChi = {
+        //     thanhPho: "Lạng Sơn",
+        //     phuong: "Na Dương",
+        //     duong: "ABC",
+        //     sonha: 123
+        // }
+
+        // // thongTinSinhVien.diaChi=diaChiNhaBan;
+
+        // document.writeln("Xin chào bạn: " + thongTinSinhVien.hoTen 
+        //     + " " + "tuổi" 
+        //     + thongTinSinhVien.tuoi 
+        //     + " " + "Nhà ở: " 
+        //     + " " + diaChi.thanhPho + "<br>"
+        // );
+
+        // let namNay = 2025;
+        // document.writeln("Bạn " + thongTinSinhVien.hoTen 
+        //     + " Sinh năm "
+        //     + (namNay - thongTinSinhVien.tuoi)
+        // );
